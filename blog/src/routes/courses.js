f3e6b15ad19d3275c230386632e6c72c840fe679
PhamTrungHieu20204968 <phamhieu15082002@gmail.com>
2023-03-29 17:50:27 +0700

    Day 7

--- blog/src/routes/courses.js
+++ blog/src/routes/courses.js
@@ -6,6 +6,7 @@ const courseController = require("../app/controllers/CourseController");
 router.get("/create", courseController.create);
 router.post("/store", courseController.store);
 router.put("/:id", courseController.update);
+router.delete("/:id", courseController.delete);
 router.get("/:id/edit", courseController.edit);
 router.get("/:slug", courseController.show);
 
