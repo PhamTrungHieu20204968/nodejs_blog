55714d2b1e092e67fd27d46a09eac45deb058556
PhamTrungHieu20204968 <phamhieu15082002@gmail.com>
2023-04-02 10:50:02 +0700

    Day 9

--- blog/src/app/controllers/MeController.js
+++ blog/src/app/controllers/MeController.js
@@ -4,7 +4,15 @@ const { mutipleMongooseToObject } = require("../../util/mongoose");
 class MeController {
   // [GET] / /me/stores/courses
   storedCourses(req, res, next) {
-    Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+    let courseQuery = Course.find({});
+
+    if (req.query.hasOwnProperty("_sort")) {
+      courseQuery = courseQuery.sort({
+        [req.query.column]: req.query.type,
+      });
+    }
+
+    Promise.all([courseQuery, Course.countDocumentsDeleted()])
       .then(([courses, deletedCount]) => {
         res.render("me/stored-courses", {
           deletedCount,
--- blog/src/index.js
+++ blog/src/index.js
@@ -3,6 +3,8 @@ const path = require("path");
 const morgan = require("morgan");
 const methodOverride = require("method-override");
 const handlebars = require("express-handlebars");
+const SortMiddleware = require("./app/middlewares/SortMiddleware");
+
 const app = express();
 const port = 3000;
 
@@ -16,6 +18,9 @@ app.use(methodOverride("_method"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
+// Custom middleware
+app.use(SortMiddleware);
+
 // HTTP logger
 app.use(morgan("combined"));
 
@@ -27,6 +32,28 @@ app.engine(
     extname: ".hbs",
     helpers: {
       sum: (a, b) => a + b,
+      sortable: (field, sort) => {
+        const sortType = field === sort.column ? sort.type : "default";
+
+        const icons = {
+          default: "oi oi-elevator",
+          asc: "oi oi-sort-ascending",
+          desc: "oi oi-sort-descending",
+        };
+
+        const types = {
+          default: "desc",
+          asc: "desc",
+          desc: "asc",
+        };
+
+        const icon = icons[sortType];
+        const type = types[sortType];
+
+        return `<a href="?_sort&column=${field}&type=${type}">
+        <span class="${icon}"></span>
+      </a>`;
+      },
     },
   })
 );
